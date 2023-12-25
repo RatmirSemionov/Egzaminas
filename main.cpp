1034de5dcb55f1ac52ed2e6c33fdf0b628ec55f3
RatmirSemionov <144428344+RatmirSemionov@users.noreply.github.com>
2023-12-25 23:58:42 +0200

    Pasirinkimas

--- main.cpp
+++ main.cpp
@@ -2,13 +2,25 @@
 
 int main()
 {
+    char Pasirinkimas;
     string inputFileName, outputFileName;
-    cout << "Iveskite failo pavadinima: ";
-    cin >> inputFileName;
-    cout << "Iveskite failo pavadinima i kuri norite issaugot rezultatus: ";
-    cin >> outputFileName;
 
-    countWords(inputFileName, outputFileName);
+    cout << "Ar norite pamatyti zodzius tekste, kurie kartojasi daugiau nei viena karta? (T - taip, N - ne) " << endl;
+    cin >> Pasirinkimas;
+    if (Pasirinkimas == 'T') {
+        cout << "Iveskite failo pavadinima su jusu teksta: ";
+        cin >> inputFileName;
+        cout << "Iveskite failo pavadinima i kuri norite issaugot rezultatus: ";
+        cin >> outputFileName;
+        countWords(inputFileName, outputFileName);
+    }
+    else {
+        cout << "Iveskite failo pavadinima su jusu teksta: ";
+        cin >> inputFileName;
+        cout << "Iveskite failo pavadinima i kuri norite issaugot rezultatus: ";
+        cin >> outputFileName;
+        findWordLocations(inputFileName, outputFileName);
+    }
 
     return 0;
 }
