c24a7f851feda4db4fd74b7f3878c86fbf6981dd
RatmirSemionov <144428344+RatmirSemionov@users.noreply.github.com>
2023-12-25 20:20:50 +0200

    Naujos funkcijos

--- mylib.h
+++ mylib.h
@@ -10,6 +10,8 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <locale>
+#include <unordered_map>
 
 using std::cout;
 using std::string;
@@ -18,9 +20,14 @@ using std::endl;
 using std::ifstream;
 using std::map;
 using std::ofstream;
+using std::setw;
+using std::right;
+using std::left;
 
 void countWords(const string& inputFileName, const string& outputFileName);
 bool isNumber(const string& s);
-string toLower(const string& s);
+string toLowerLT(const string& s);
+bool isCustomPunct(char c);
+int UTF8(const string& str);
 
 #endif // MYLIB_H_INCLUDED
